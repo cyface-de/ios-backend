07045b846d3b26af04b0b30f445f253e8870d55b
Klemens Muthmann <klemens_muthmann@gmx.de>
2017-11-07 10:50:11 +0100

    Initial Commit

---
+++ DataCapturing/DataCapturing.h
@@ -0,0 +1,19 @@
+//
+//  DataCapturing.h
+//  DataCapturing
+//
+//  Created by Team Cyface on 07.11.17.
+//  Copyright © 2017 Cyface GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataCapturing.
+FOUNDATION_EXPORT double DataCapturingVersionNumber;
+
+//! Project version string for DataCapturing.
+FOUNDATION_EXPORT const unsigned char DataCapturingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataCapturing/PublicHeader.h>
+
+
