cd960ecf2d768cadae707f7f49d7d945edcc848f
Klemens Muthmann <klemens_muthmann@gmx.de>
2017-12-05 15:21:47 +0100

    Adds support for saving and loading measurements
    This commit ports the old in-memory data model to a data model using the CoreData framework to store entities. That way it is possible to keep measurements during app restarts. For this purpose we extended the DataCapturing framework by a CoreData data model. This is not an easy task, since loading a data model from a framework is not as straight forward as from a stand alone app. Luckily there is a question on StackOverflow with an appropriate solution (CAREFUL: The second answer is the important one!) https://stackoverflow.com/questions/42553749/core-data-failed-to-load-model.

--- DataCapturing/DataCapturingService.swift
+++ DataCapturing/DataCapturingService.swift
@@ -10,6 +10,7 @@ import Foundation
 import CoreMotion
 import CoreLocation
 import os.log
+import CoreData
 
 /**
  An object of this class handles the lifecycle of starting and stopping data capturing as well as transmitting results to an appropriate server.
@@ -34,14 +35,16 @@ public class DataCapturingService: NSObject, CLLocationManagerDelegate {
      */
     private var listener: DataCapturingListener?
     
+    /*
     /**
      A poor mans data storage.
      
      This is only in memory and will be replaced by a database on persistent storage during final implementation.
      */
-    private(set) public var unsyncedMeasurements: [Measurement]
+    private(set) public var unsyncedMeasurements: [MeasurementMO]
+ */
     
-    private var currentMeasurement: Measurement?
+    private var currentMeasurement: MeasurementMO?
     
     /**
      An instance of `CMMotionManager`. There should be only one instance of this type in your application.
@@ -60,6 +63,9 @@ public class DataCapturingService: NSObject, CLLocationManagerDelegate {
         return manager
     }()
     
+    private let persistenceLayer: PersistenceLayer
+
+    
     //MARK: Initializers
     /**
      Creates a new completely initialized `DataCapturingService`.
@@ -67,8 +73,9 @@ public class DataCapturingService: NSObject, CLLocationManagerDelegate {
      - motionManager: An instance of `CMMotionManager`. There should be only one instance of this type in your application. Since it seems to be impossible to create that instance inside a framework at the moment, you have to provide it via this parameter.
      - interval: The accelerometer update interval in Hertz.
      */
-    public init(withManager motionManager:CMMotionManager, andUpdateInterval interval : Double) {
-        unsyncedMeasurements = []
+    public init(withManager motionManager:CMMotionManager, andUpdateInterval interval : Double, using persistence: PersistenceLayer) {
+        //unsyncedMeasurements = [] // TODO init persistence layer here and load unsynced measurements
+        self.persistenceLayer = persistence
         isRunning = false
         self.motionManager = motionManager
         motionManager.accelerometerUpdateInterval = 1.0 / interval
@@ -86,9 +93,10 @@ public class DataCapturingService: NSObject, CLLocationManagerDelegate {
         
         self.locationManager.startUpdatingLocation()
         self.isRunning = true
-        let measurement = Measurement(Int64(unsyncedMeasurements.count))
+        let measurement = persistenceLayer.createMeasurement(at: currentTimeInMillisSince1970())
+        // TODO Create persistent measurement here
         self.currentMeasurement = measurement
-        self.unsyncedMeasurements.append(measurement)
+        //self.unsyncedMeasurements.append(measurement)
         
         if(motionManager.isAccelerometerAvailable) {
             motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { data, error in
@@ -98,8 +106,8 @@ public class DataCapturingService: NSObject, CLLocationManagerDelegate {
                 
                 let accValues = myData.acceleration
                 //let eventDate = NSDate(timeInterval: myData.timestamp, sinceDate: bootTime)
-                let acc = AccelerationPoint(id: nil, ax: accValues.x,ay: accValues.y, az: accValues.z,timestamp: self.currentTimeInMillisSince1970())
-                measurement.append(acc)
+                let acc = self.persistenceLayer.createAcceleration(x: accValues.x,y: accValues.y, z: accValues.z,at: self.currentTimeInMillisSince1970())
+                measurement.addToAccelerations(acc)
             }
         }
     }
@@ -128,17 +136,15 @@ public class DataCapturingService: NSObject, CLLocationManagerDelegate {
      Forces the service to synchronize all Measurements now if a connection is available. If this is not called the service might wait for an opprotune moment to start synchronization.
      */
     public func forceSync() {
-        unsyncedMeasurements.removeAll()
+        // TODO add transmission code.
+        persistenceLayer.deleteMeasurements()
     }
     
     /**
      Deletes an unsynchronized `Measurement` from this device.
      */
-    public func delete(unsynced measurement : Measurement) {
-        guard let index = unsyncedMeasurements.index(of:measurement) else {
-            return
-        }
-        unsyncedMeasurements.remove(at:index)
+    public func delete(unsynced measurement : MeasurementMO) {
+        persistenceLayer.delete(measurement: measurement)
     }
     
     /**
@@ -164,6 +170,10 @@ public class DataCapturingService: NSObject, CLLocationManagerDelegate {
         guard let measurement = currentMeasurement else {
             fatalError("No current measurement to save the location to! Data capturing impossible.")
         }
-        measurement.append(GeoLocation(lat: location.coordinate.latitude,lon: location.coordinate.longitude,speed: location.speed,accuracy: location.horizontalAccuracy,timestamp: convertToUtcTimestamp(date: location.timestamp)))
+        let geoLocation = persistenceLayer.createGeoLocation(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude, accuracy: location.horizontalAccuracy, speed: location.speed, at: convertToUtcTimestamp(date: location.timestamp))
+        measurement.addToGeoLocations(geoLocation)
+        
+        
+        persistenceLayer.save()
     }
 }
---
+++ DataCapturing/Model/MeasurementMO+CoreDataClass.swift
@@ -0,0 +1,29 @@
+//
+//  MeasurementMO+CoreDataClass.swift
+//  
+//
+//  Created by Team Cyface on 04.12.17.
+//
+//
+
+import Foundation
+import CoreData
+
+/**
+ An object of this class represents a single measurement captured by the `DataCapturingService`.
+ 
+ - Author:
+ Klemens Muthmann
+ 
+ - Version:
+ 2.0.0
+ 
+ - Since:
+ 1.0.0
+ 
+ This usually happens between complementary calls to `DataCapturingService#startCapturing()` and `DataCapturingService#stopCapturing()`.
+ */
+@objc(MeasurementMO)
+public class MeasurementMO: NSManagedObject {
+
+}
