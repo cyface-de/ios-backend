0c0bdeed617c030de0729e34775aabf1f2cdfb17
Klemens Muthmann <klemens_muthmann@gmx.de>
2017-12-13 08:25:56 +0100

    Adds documentation and refactors event processing
    This commit adds and changes some of the documentation to comply with Apples API design guidelines. It also begins to change the event system from a listener based variant to a more Swift like closure based one.

---
+++ DataCapturing/DataCapturingEvents.swift
@@ -0,0 +1,37 @@
+//
+//  DataCapturingEvents.swift
+//  DataCapturing
+//
+//  Created by Team Cyface on 13.12.17.
+//  Copyright © 2017 Cyface GmbH. All rights reserved.
+//
+
+import Foundation
+
+/**
+ Events occuring during capturing and transmitting data.
+ 
+ These events may be received by a closure provided to the `DataCapturingService` on calling start.
+ 
+ - Author: Klemens Muthmann
+ - Version: 1.0.0
+ - Since: 1.0.0
+ */
+public enum DataCapturingEvents {
+    /// Occurs everytime the `DataCapturingService` received a geo location fix and thus is able to track its position.
+    case geoLocationFixAcquired
+    /// Occurs everytime the `DataCapturingService` loses its geo location fix.
+    case geoLocationFixLost
+    /**
+     Occurs each time the `DataCapturingService` receives a new geo location position.
+     - position: The new geo location position.
+     */
+    case geoLocationAcquired(position: GeoLocation)
+    /**
+     Occurs each time the application runs out of space. How much space is used and how much is available may be retrieved from `allocation`.
+     - allocation: Information about the applications disk (or rather SD card) space consumption.
+     */
+    case lowDiskSpace(allocation: DiskConsumption)
+    /// Occurs if the `DataCapturingService` has synchronized all pending cached data successfully and deleted the local copies.
+    case synchronizationSuccessful
+}
--- DataCapturing/DataCapturingService.swift
+++ DataCapturing/DataCapturingService.swift
@@ -14,40 +14,34 @@ import os.log
 /**
  An object of this class handles the lifecycle of starting and stopping data capturing as well as transmitting results to an appropriate server.
  
- - Author: Klemens Muthmann
- - Version: 1.0.0
- - Since: 1.0.0
- 
  To avoid using the users traffic or incurring costs, the service waits for Wifi access before transmitting any data. You may however force synchronization if required, using `forceSyncU()`.
  
  An object of this class is not thread safe and should only be used once per application. Youmay start and stop the service as often as you like and reuse the object.
- */
-public class DataCapturingService: NSObject, CLLocationManagerDelegate {
+ 
+ - Author: Klemens Muthmann
+ - Version: 2.0.0
+ - Since: 1.0.0
+  */
+public class DataCapturingService: NSObject  {
     //MARK: Properties
-    /**
-     `true` if data capturing is running; `false` otherwise.
-     */
+    /// `true` if data capturing is running; `false` otherwise.
     private(set) public var isRunning: Bool
     
-    /**
-     A listener that is notified of important events during data capturing.
-     */
+    /// A listener that is notified of important events during data capturing.
     private var listener: DataCapturingListener?
     
-    /**
-     A poor mans data storage.
-     
-     This is only in memory and will be replaced by a database on persistent storage during final implementation.
-     */
-    private(set) public var unsyncedMeasurements: [Measurement]
-    
+    /// The currently recorded `Measurement` or nil if there is no active recording.
     private var currentMeasurement: Measurement?
     
-    /**
-     An instance of `CMMotionManager`. There should be only one instance of this type in your application.
-     */
+    /// An instance of `CMMotionManager`. There should be only one instance of this type in your application.
     private let motionManager: CMMotionManager
     
+    ///
+    private lazy var dataUploadSession: URLSession = {
+        return URLSession()
+    }()
+    
+    /// Provides access to the devices geo location capturing hardware (such as GPS, GLONASS, GALILEO, etc.) and handles geo location updates in the background.
     private lazy var locationManager: CLLocationManager = {
         let manager = CLLocationManager()
         manager.delegate = self
@@ -62,26 +56,26 @@ public class DataCapturingService: NSObject, CLLocationManagerDelegate {
     
     //MARK: Initializers
     /**
-     Creates a new completely initialized `DataCapturingService`.
-     - parameters:
-     - motionManager: An instance of `CMMotionManager`. There should be only one instance of this type in your application. Since it seems to be impossible to create that instance inside a framework at the moment, you have to provide it via this parameter.
-     - interval: The accelerometer update interval in Hertz.
+     Creates a new completely initialized `DataCapturingService` transmitting data to a provided endpoint and accessing data a certain amount of times per second.
+     - Parameters:
+     - dataEndpoint: A `URL` used to upload data to. There should be a server complying to a Cyface REST interface available at that location.
+     - sensorManager: An instance of `CMMotionManager`. There should be only one instance of this type in your application. Since it seems to be impossible to create that instance inside a framework at the moment, you have to provide it via this parameter.
+     - updateInterval: The accelerometer update interval in Hertz. By default this is set to the supported maximum of 100 Hz.
      */
-    public init(withManager motionManager:CMMotionManager, andUpdateInterval interval : Double) {
+    public init(dataEndpoint endpoint: URL, sensorManager manager:CMMotionManager, updateInterval interval : Double = 100) {
         unsyncedMeasurements = []
         isRunning = false
-        self.motionManager = motionManager
+        self.motionManager = manager
         motionManager.accelerometerUpdateInterval = 1.0 / interval
         super.init()
     }
     
     //MARK: Methods
-    /**
-     Starts the capturing process. This operation is idempotent.
-     */
+    /// Starts the capturing process. This operation is idempotent.
     public func start() {
         guard !isRunning else {
-            fatalError("Trying to start DataCapturingService which is already running!")
+            os_log("Trying to start DataCapturingService which is already running!")
+            return
         }
         
         self.locationManager.startUpdatingLocation()
@@ -107,7 +101,7 @@ public class DataCapturingService: NSObject, CLLocationManagerDelegate {
     /**
      Starts the capturing process with a listener that is notified of important events occuring while the capturing process is running. This operation is idempotent.
      
-     - parameters:
+     - Parameters:
      - listener: A listener that is notified of important events during data capturing.
      */
     public func start(with listener:DataCapturingListener) {
@@ -115,25 +109,21 @@ public class DataCapturingService: NSObject, CLLocationManagerDelegate {
         self.start()
     }
     
-    /**
-     Stops the currently running data capturing process or does nothing of the process is not running.
-     */
+    /// Stops the currently running data capturing process or does nothing of the process is not running.
     public func stop() {
         isRunning = false
         motionManager.stopAccelerometerUpdates()
         locationManager.stopUpdatingLocation()
+        currentMeasurement = nil
     }
     
-    /**
-     Forces the service to synchronize all Measurements now if a connection is available. If this is not called the service might wait for an opprotune moment to start synchronization.
-     */
+    /// Forces the service to synchronize all Measurements now if a connection is available. If this is not called the service might wait for an opprotune moment to start synchronization.
     public func forceSync() {
         unsyncedMeasurements.removeAll()
+        let url
     }
     
-    /**
-     Deletes an unsynchronized `Measurement` from this device.
-     */
+    /// Deletes an unsynchronized `Measurement` from this device.
     public func delete(unsynced measurement : Measurement) {
         guard let index = unsyncedMeasurements.index(of:measurement) else {
             return
@@ -141,18 +131,19 @@ public class DataCapturingService: NSObject, CLLocationManagerDelegate {
         unsyncedMeasurements.remove(at:index)
     }
     
-    /**
-     Provides the current time in milliseconds since january 1st 1970 (UTC).
-     */
+    /// Provides the current time in milliseconds since january 1st 1970 (UTC).
     private func currentTimeInMillisSince1970() -> Int64 {
         return convertToUtcTimestamp(date: Date())
     }
     
+    /// Converts a `Data` object to a UTC milliseconds timestamp since january 1st 1970.
     private func convertToUtcTimestamp(date value: Date) -> Int64 {
         return Int64(value.timeIntervalSince1970*1000.0)
     }
-    
-    // MARK: CLLocationManagerDelegate
+}
+
+// MARK: CLLocationManagerDelegate
+extension DataCapturingService: CLLocationManagerDelegate {
     public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         guard !locations.isEmpty else {
@@ -167,3 +158,4 @@ public class DataCapturingService: NSObject, CLLocationManagerDelegate {
         measurement.append(GeoLocation(lat: location.coordinate.latitude,lon: location.coordinate.longitude,speed: location.speed,accuracy: location.horizontalAccuracy,timestamp: convertToUtcTimestamp(date: location.timestamp)))
     }
 }
+// TODO: Maybe move out the data transmission part to its own class. This seems to be mingled up here with the data capturing part.
