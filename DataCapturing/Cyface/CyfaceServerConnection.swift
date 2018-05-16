6eb0fbeb8397c637e76a3bce735f86d3f2e845e3
Klemens Muthmann <klemens.muthmann@gmail.com>
2018-05-16 11:27:02 +0200

    Refactors all network traffic to use Alamofire
    This commit uses Alamofire in the CyfaceServerConnection and makes sure authentication works properly. Everything else needs to be tested agains the real test server.

--- DataCapturing/Cyface/CyfaceServerConnection.swift
+++ DataCapturing/Cyface/CyfaceServerConnection.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import os.log
+import Alamofire
 
 // TODO: The parameter for the finished handlers is called differently all the time
 // TODO: Parameter order in the finished handler is not the same always (error is sometimes the first and sometimes the last parameter
@@ -40,7 +41,7 @@ public class CyfaceServerConnection: ServerConnection {
     /**
      The handler to call after authentication has been finished. If an error occurs the error property of this handler contains further details. This property is `nil` after successful authentication.
     */
-    private var onAuthenticationFinishedHandler: ((Error?) -> Void)?
+    //private var onAuthenticationFinishedHandler: ((Error?) -> Void)?
 
     /**
      The world wide unique identifier of this app installation. The name is a bit misleading. It is used to identify multiple uploads from the same device, for example to create reusable machine learning models for one or only some devices. If the app is uninstalled and reinstalled, this identifier is reset to a new value. It is generated and registered with the server on the first upload.
@@ -79,21 +80,26 @@ public class CyfaceServerConnection: ServerConnection {
         - onFinish: A handler called after authentication has been finished. If an error occured, the provided parameter contains further details. If authentication was successful, that parameter is `nil`.
     */
     public func authenticate(with username: String, and password: String, onFinish handler: ((Error?) -> Void)?) {
-        let loginURL = apiURL.appendingPathComponent("login")
-        var request = URLRequest(url: loginURL)
-        self.onAuthenticationFinishedHandler = handler
-        request.httpMethod = "POST"
-        request.setValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-Type")
-        guard let body = try? JSONSerialization.data(withJSONObject: [
-            "login": "\(username)",
-            "password": "\(password)"]) else {
-            fatalError("authenticate(username: \(username), password: \(password)): Unable to encode username and password into a JSON request.")
-        }
-        request.httpBody = body
 
         if jwtBearer==nil {
-            let authenticationTask = apiSession.dataTask(with: request, completionHandler: onAuthenticationResponse)
-            authenticationTask.resume()
+            let body = [
+                "login": "\(username)",
+                "password": "\(password)"]
+
+            Alamofire.request(apiURL.appendingPathComponent("login"), method: .post, parameters: body, encoding: JSONEncoding.default).validate(statusCode: [200]).response { (response) in
+                if let handler = handler {
+                    if let error = response.error {
+                        handler(error)
+                    } else if let jwtBearer = response.response?.allHeaderFields["Authorization"] as? String {
+                        self.jwtBearer = jwtBearer
+                        handler(nil)
+                    } else {
+                        handler(ServerConnectionError(
+                            title: "Authentication Error",
+                            description: "No Authorization token received from Cyface API available at \(self.apiURL)."))
+                    }
+                }
+            }
         }
     }
 
@@ -141,41 +147,28 @@ public class CyfaceServerConnection: ServerConnection {
     */
     private func transmit(measurement: MeasurementEntity, forDevice deviceIdentifier: String, onFinish handler: @escaping (MeasurementEntity, ServerConnectionError?) -> Void) {
 
-        var request = URLRequest(url: apiURL.appendingPathComponent("measurements"))
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue(jwtBearer, forHTTPHeaderField: "Authorization")
-
-        makeUploadChunks(fromMeasurement: measurement, forInstallation: deviceIdentifier) { chunk in
+        makeUploadChunks(fromMeasurement: measurement, forInstallation: deviceIdentifier) { [unowned self] chunk in
             guard let jsonChunk = try? JSONSerialization.data(withJSONObject: chunk, options: .sortedKeys) else {
                 fatalError("ServerConnection.transmit(measurement: \(measurement.identifier), forDevice: \(deviceIdentifier)): Invalid measurement format.")
             }
 
-            let submissionTask = self.apiSession.uploadTask(with: request, from: jsonChunk) { _, response, error in
-                if let error = error {
+            guard let jwtBearer = self.jwtBearer else {
+                fatalError("ServerConnection.transmit(measurement: \(measurement.identifier), forDevice: \(deviceIdentifier)): No JWT auth token available to upload data.")
+            }
+
+            let headers: HTTPHeaders = [
+                "Authorization": jwtBearer,
+                "Content-Type": "application/json"]
+            Alamofire.upload(jsonChunk, to: self.apiURL.appendingPathComponent("measurements").absoluteString, method: .post, headers: headers).validate(statusCode: [201]).response { (response) in
+
+                if let error = response.error {
                     handler(measurement, ServerConnectionError(
                         title: "Data Transmission Error",
                         description: "Error while transmitting data to the server at \(self.apiURL)! Error was: \(error)"))
-                    return
+                } else {
+                    handler(measurement, nil)
                 }
-                guard let response = response as? HTTPURLResponse else {
-                    handler(measurement, ServerConnectionError(
-                        title: "Data Transmission Error",
-                        description: "Received erroneous response from \(String(describing: request.url))!"))
-                    return
-                }
-
-                let statusCode = response.statusCode
-                guard statusCode == 201 else {
-                    handler(measurement, ServerConnectionError(
-                        title: "Invalid Status Code Error",
-                        description: "Invalid status code \(statusCode) received from server at \(String(describing: request.url))!"))
-                    return
-                }
-
-                handler(measurement, nil)
             }
-            submissionTask.resume()
         }
     }
 
@@ -297,35 +290,16 @@ public class CyfaceServerConnection: ServerConnection {
                 """)
         }
 
-        var request = URLRequest(url: self.apiURL.appendingPathComponent("devices"))
-        request.httpMethod = "GET"
-        request.setValue(jwtBearer, forHTTPHeaderField: "Authorization")
-
-        let deviceRetrievalTask = self.apiSession.dataTask(with: request) { data, response, error in
-            if let error = error {
+        let headers: HTTPHeaders = ["Authorization": jwtBearer!]
+        Alamofire.request(self.apiURL.appendingPathComponent("devices"), headers: headers).validate(statusCode: [200]).response { (response) in
+            if let error = response.error {
                 handler(ServerConnectionError(
                     title: "Device Registration Error",
                     description: "Server error while checking if server knows me!, Error \(error)"), false)
                 return
             }
 
-            guard let response = response as? HTTPURLResponse else {
-                handler(ServerConnectionError(
-                    title: "Device Registration Error",
-                    description: "Invalid reponse received while checking for registered device"), false)
-                return
-            }
-
-            guard response.statusCode == 200 else {
-                handler(ServerConnectionError(
-                    title: "Device Registration Error",
-                    description: """
-                    Invalid status code while checking if device exists. Status Code: \(response.statusCode)
-                    """), false)
-                return
-            }
-
-            guard let data = data else {
+            guard let data = response.data else {
                 handler(ServerConnectionError(
                     title: "Device Registration Error",
                     description: "Unable to unwrap server response from checking for existing device."), false)
@@ -340,10 +314,7 @@ public class CyfaceServerConnection: ServerConnection {
             }
 
             handler(nil, foundDevice)
-
         }
-
-        deviceRetrievalTask.resume()
     }
 
     /**
@@ -365,95 +336,23 @@ public class CyfaceServerConnection: ServerConnection {
                 """)
         }
 
-        var request = URLRequest(url: self.apiURL.appendingPathComponent("devices"))
-        request.httpMethod = "POST"
-        request.setValue(jwtBearer, forHTTPHeaderField: "Authorization")
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let headers: HTTPHeaders = [
+            "Authorization": jwtBearer!,
+            "Content-Type": "application/json"
+        ]
+        let parameters = [
+            "id": identifier,
+            "name": deviceModelIdentifier
+        ]
 
-        let deviceCreationBody = ["id": identifier, "name": deviceModelIdentifier]
-        request.httpBody = try? JSONSerialization.data(withJSONObject: deviceCreationBody, options: .sortedKeys)
-
-        let deviceCreationTask = self.apiSession.dataTask(with: request) { _, response, error in
-            if let error = error {
+        Alamofire.request(self.apiURL.appendingPathComponent("devices"), method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).validate(statusCode: [201]).response { (response) in
+            if let error = response.error {
                 handler(ServerConnectionError(
                     title: "Device Registration Error",
-                    description: "Server error during device registration!, Error \(error)"), identifier)
-                return
+                    description: "Error during device registration!, Error \(error)"), identifier)
+            } else {
+                handler(nil, identifier)
             }
-
-            guard let response = response as? HTTPURLResponse else {
-                handler(ServerConnectionError(
-                    title: "Device Registration Error",
-                    description: """
-                    Unable to unwrap response received after trying to register device with \(self.apiURL)).
-                    """), identifier)
-                return
-            }
-
-            guard response.statusCode == 201 else {
-                handler(ServerConnectionError(
-                    title: "Device Registration Error",
-                    description: """
-                    Invalid response status code \(response.statusCode) from server \(String(describing: request.url))!
-                    """), identifier)
-                return
-            }
-
-            handler(nil, identifier)
-        }
-
-        deviceCreationTask.resume()
-    }
-
-    /**
-     A Handler called when a request for authentication returns.
-
-     - Parameters:
-        - response: The response received as reaction to the request.
-        - error: An optional error parameter that is `nil` if the response was successful.
-
-     - SeeAlso `authenticate(with:and:onFinish)`
-    */
-    private func onAuthenticationResponse(_: Data?, response: URLResponse?, error: Error?) {
-        guard let handler = onAuthenticationFinishedHandler else {
-            fatalError("No handler for authentication finished event available!")
-        }
-
-        if let error = error {
-            handler(error)
-            return
-        }
-
-        guard let unwrappedResponse = response as? HTTPURLResponse else {
-            handler(ServerConnectionError(
-                title: "Authentication Error",
-                description: """
-                There has been a client side error while authenticating with the Cyface API available at \(self.apiURL).
-                """))
-            return
-        }
-
-        let statusCode = unwrappedResponse.statusCode
-        let authenticationToken = unwrappedResponse.allHeaderFields["Authorization"] as? String
-
-        guard let unwrappedAuthenticationToken = authenticationToken else {
-            handler(ServerConnectionError(
-                title: "Authentication Error",
-                description: "No Authorization token received from Cyface API available at \(self.apiURL)."))
-            return
-        }
-
-        if statusCode == 200 {
-            self.jwtBearer = unwrappedAuthenticationToken
-            handler(nil)
-        } else {
-            handler(ServerConnectionError(
-                title: "Authentication Error",
-                description: """
-                There has been a client side error while authenticating with the Cyface API
-                available at \(self.apiURL).
-                The call provided the following status code \(statusCode).
-                """))
         }
     }
 }
