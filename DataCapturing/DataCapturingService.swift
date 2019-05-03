1dd58011f56e49ef505a05c11db920e45d1c6012
Klemens Muthmann <klemens.muthmann@gmail.com>
2019-05-03 17:23:46 +0200

    Check for correct value in locationUpdateSkipRate

--- DataCapturing/DataCapturingService.swift
+++ DataCapturing/DataCapturingService.swift
@@ -45,7 +45,13 @@ public class DataCapturingService: NSObject {
     public var currentMeasurement: MeasurementEntity?
 
     /// Locations are captured approximately once per second on most devices. If you would like to get fewer updates this parameter controls, how many events are skipped before one is reported to your handler. The default value is 1, which reports every event. To receive fewer events you could for example set it to 5 to only receive every fifth event.
-    public var locationUpdateSkipRate = 1
+    public var locationUpdateSkipRate: UInt = 1 {
+        willSet(newValue) {
+            if(newValue==0) {
+                fatalError("Invalid value 0 for locationUpdateSkipRate!")
+            }
+        }
+    }
 
     /**
      Provides access to the devices geo location capturing hardware (such as GPS, GLONASS, GALILEO, etc.)
