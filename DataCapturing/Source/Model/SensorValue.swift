84ff80029fbf1421588f532885d4bfdff07113ec
Klemens Muthmann <klemens.muthmann@cyface.de>
2021-09-24 11:29:45 +0200

    [STAD-258] Fix wrong timestamps on sensor data (#119)
    * Add test for capturing sensor data
    
    * Make data points depend on current time again
    
    We had changed this to kernel boot time, which does not acutally work,
    since the time reported via the sensor system is not continuous but
    broken up in between by device sleeps.
    
    * Remove unusable test
    
    * Add missing documentation
    
    * Add check to filter late sensor values
    
    The removed test case was not properly deleted from the project file.
    This commit achieves this.
    
    * Fix intendation in SensorCapturer
    
    * Add missing remark to SensorCapturer documentation
    
    * Fix error handling of sensor event handlers
    
    Previously the error handling would always fail and even in cases of
    errors not leave the handler. This can lead to crashes if an error
    occurs. Now errors should lead to the app gracefully ignoring that
    sensor value.
    
    * Add explanation why Date() is used for timestamp calculation

--- DataCapturing/Source/Model/SensorValue.swift
+++ DataCapturing/Source/Model/SensorValue.swift
@@ -31,7 +31,7 @@ import Foundation
  - Since: 1.0.0
  - Note: This was called  `Acceleration` in previous versions of the SDK.
  */
-public class SensorValue {
+public class SensorValue: CustomStringConvertible {
 
     // MARK: - Properties
 
@@ -46,6 +46,9 @@ public class SensorValue {
 
     /// Value in the device z direction, which is standing on the screen pointing towards you if the homebutton faces you.
     public let z: Double
+
+    /// A human readable description of this object. This is required for debugging purposes.
+    public var description: String { return "Sensor Value: (timestamp: \(timestamp), x: \(x), y: \(y), z: \(z))" }
 
     // MARK: - Initializers
 
