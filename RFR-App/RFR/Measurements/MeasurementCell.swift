23b1e7ccfa6549c6e70f39ba98569c0b99bf0d63
Klemens Muthmann <klemens.muthmann@gmail.com>
2023-09-12 18:44:59 +0200

    Add missing License headers

--- RFR-App/RFR/Measurements/MeasurementCell.swift
+++ RFR-App/RFR/Measurements/MeasurementCell.swift
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with the Read-for-Robots iOS App. If not, see <http://www.gnu.org/licenses/>.
  */
-
 import SwiftUI
 import MapKit
 
